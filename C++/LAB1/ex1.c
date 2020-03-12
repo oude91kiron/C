7fb453aac2cc6b105f359c5bbc920cca08d628f8
oude91kiron <oa45@students.kiron.ngo>
2020-03-13 02:36:42 +0300

    update file

--- C++/LAB1/ex1.c
+++ C++/LAB1/ex1.c
@@ -1,23 +1,39 @@
-#include <iostream>
+/* Write a program in C++ to make such a pattern like a pyramid with numbers increased by 1.
+   1
+  2 3
+ 4 5 6
+7 8 9 10
+*/
+
+#include <iostream> // input/output strem
 
 using namespace std;
 
 int main()
 {
-    int rows=9;
-    int i,j, k=0;
-    for (i=1; i<=rows; ++i,k=0){
-        for (j=1; j<=rows-i; ++j)
+    int space=3;
+    int val=1;
+    int rows=4;
+
+    for(int i=1;i<=rows;i++)
+    {
+        for(int s=1;s<=space;s++)
+            cout<<" ";
+
+        space--;
+
+
+        for(int n=1;n<=i;n++)
         {
-            cout << " ";
+            cout<<val<<" ";
+            val++;
         }
 
-        while (k!=2*i-1)
-        {
-            cout << "*";
-            ++k;
-        }
-        cout << "\n";
+
+        cout<<"\n";
     }
+
+
+
     return 0;
 }
